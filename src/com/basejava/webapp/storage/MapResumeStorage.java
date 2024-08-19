02c95bf0f75d4254b6db1fe78338db280041fb9a
raketavlad <zerosworld@mail.ru>
2024-08-19 19:56:23 +0300

    Шестой урок - Второе исправление домашнего задания

--- src/com/basejava/webapp/storage/MapResumeStorage.java
+++ src/com/basejava/webapp/storage/MapResumeStorage.java
@@ -2,21 +2,15 @@ package com.basejava.webapp.storage;
 
 import com.basejava.webapp.model.Resume;
 
-import java.util.ArrayList;
-import java.util.LinkedHashMap;
-import java.util.List;
+import java.util.*;
 
 public class MapResumeStorage extends AbstractStorage {
 
-    private final LinkedHashMap<String, Resume> storage = new LinkedHashMap<>();
+    private final Map<String, Resume> storage = new LinkedHashMap<>();
 
     @Override
     protected List<Resume> getList() {
-        List<Resume> listResume = new ArrayList<>(storage.size());
-        for (String key : storage.keySet()) {
-            listResume.add(storage.get(key));
-        }
-        return listResume;
+        return Arrays.asList(storage.values().toArray(new Resume[0]));
     }
 
     @Override
@@ -36,12 +30,12 @@ public class MapResumeStorage extends AbstractStorage {
 
     @Override
     protected void doSave(Resume resume, Object searchKey) {
-        storage.put(((Resume) searchKey).getUuid(), resume);
+        storage.put(resume.getUuid(), resume);
     }
 
     @Override
     protected Resume doGet(Object searchKey) {
-        return storage.get(((Resume) searchKey).getUuid());
+        return (Resume) searchKey;
     }
 
     @Override
@@ -51,12 +45,11 @@ public class MapResumeStorage extends AbstractStorage {
 
     @Override
     protected Object getSearchKey(String uuid) {
-        Resume searchKey = new Resume(uuid, "Random name");
-        return searchKey;
+        return storage.get(uuid);
     }
 
     @Override
     protected boolean isExist(Object searchKey) {
-        return storage.containsValue(searchKey);
+        return searchKey != null;
     }
 }
